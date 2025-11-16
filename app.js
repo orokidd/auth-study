4dae557e48bef6f5c5b3f3646071330f0944fa38
orokidd <gregomily@gmail.com>
2025-11-16 14:08:31 +0800

    passport local strategy, serialize, and deserialize function

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
 const path = require("path");
 const express = require("express");
+const { Pool } = require("pg");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const { Pool } = require("pg");
-const { configDotenv } = require("dotenv");
 
 const pool = new Pool({
   host: process.env.DB_HOST,
@@ -43,6 +42,42 @@ app.post("/sign-up", async (req, res, next) => {
     res.redirect("/");
   } catch (err) {
     return next(err);
+  }
+});
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
   }
 });
 
